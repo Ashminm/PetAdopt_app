072ac07bccc973c4f07d936112ddbcc55b7882f1
Ashmin <ashmin2802@gmail.com>
2024-05-03 10:21:17 +0530

    basrurl

--- src/service/baseUel.js
+++ src/service/baseUel.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "ashminms-projects/pet-adopt-app-server"
 
+// http://localhost:4000
 // ashminms-projects/pet-adopt-app-server
